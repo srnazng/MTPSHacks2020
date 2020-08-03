49258f22a2378072cdbcc374e2032efbfae58718
Ria Vora <riavora@rias-mbp.myfiosgateway.com>
2020-08-03 10:53:23 -0400

    ria testing heroku

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1> hello wrigaohrje</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
