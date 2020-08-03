6224aa188b18bb2d302453d399c968047c9825db
srnazng <srnazng@gmail.com>
2020-08-03 00:19:24 -0400

    final

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
